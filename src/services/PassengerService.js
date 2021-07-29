f4bf1b7b75be67f511988df5ff2a7793779f469e
ThitinatIM-kiyuu <75411172+ThitinatIM-kiyuu@users.noreply.github.com>
2021-07-29 21:08:50 +0700

    commit5.2

---
+++ src/services/PassengerService.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'https://api.instantwebtools.net/v1',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getEvents(perPage,page) {
+    return apiClient.get('/passenger?page='+ page + '&size=' + perPage)
+  },
+  //added new call
+  getpassenger(id) {
+    return apiClient.get('/passenger/' + id)
+  }
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event._id" :event="event" />
+    
   </div>
+
+  
+    
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import EventCard from '@/components/EventCard.vue'
+import PassengerService from '@/services/PassengerService.js'
+import { watchEffect } from '@vue/runtime-core'
+// import axios from 'axios'
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  name: 'EventList',
+  props: {
+    page: {
+      type: Number,
+      required: true
+    },
+    moredata: {
+      type: Number,
+      required: true
+    }
   },
-};
+  components: {
+    EventCard // register it as a child component
+  },
+  data() {
+    return {
+      events: null,
+      totalEvents: 0 //Added this to store totalEvents
+    }
+  },
+  created() {
+    watchEffect(() => {
+      PassengerService.getEvents(this.moredata, this.page)
+      .then((response) => {
+        this.events = response.data.data
+        this.totalEvents = response.headers['x-total-count'] //Store it
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      // First, calculate total page
+      let totalPages = Math.ceil(this.totalEvents / 2) //2 is events per page
+      
+      // Then check to see if the current page is less than the total pages.
+      return this.page < totalPages
+    }
+  }
+
+}
 </script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color:#2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
+}
+</style>
