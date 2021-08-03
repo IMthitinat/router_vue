7f65a73df569d2df36ce9594389f6baf71471c99
ThitinatIM-kiyuu <75411172+ThitinatIM-kiyuu@users.noreply.github.com>
2021-08-03 18:27:32 +0700

    Update Airline.vue

--- src/views/event/Airline.vue
+++ src/views/event/Airline.vue
@@ -1,26 +1,24 @@
 <template>
-   <div v-if="event">
+  <div v-if="event">
     <img :src="event.airline.logo" />
     <img
       v-for="airline in event.airline"
       :key="airline.id"
       :src="airline.logo"
     />
-        <!-- <div v-if="check"> -->
-      <h3>{{ event.airline.country }}</h3>
-      <h5>{{ event.airline.head_quaters }}</h5>
-      <p>{{ event.airline.website }}</p>
-      <h2>Slogan: {{ event.airline.slogan }}</h2>
+    <!-- <div v-if="check"> -->
+    <h3>{{ event.airline.country }}</h3>
+    <h5>{{ event.airline.head_quaters }}</h5>
+    <p>{{ event.airline.website }}</p>
     <!-- </div> -->
     <!-- <div v-if="check == false"> -->
-      <span v-for="airline in event.airline" :key="airline.id">
-        <h2>Slogan: {{ airline.slogan }}</h2>
-        <h3>{{ airline.country }}</h3>
-        <h5>{{ airline.head_quaters }}</h5>
-        <p>{{ airline.website }}</p>
-      </span>
+    <span v-for="airline in event.airline" :key="airline.id">
+      <h3>{{ airline.country }}</h3>
+      <h5>{{ airline.head_quaters }}</h5>
+      <p>{{ airline.website }}</p>
+    </span>
     <!-- </div> -->
-   </div>
+  </div>
 </template>
 
 <script>
